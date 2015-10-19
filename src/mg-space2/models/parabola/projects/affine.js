e53772d656f1cffb8b152184dd7d650c07715d39
sivsais <sivsais@gmail.com>
2015-10-20 02:35:47 +0300

    fix(parabola):

--- src/mg-space2/models/parabola/projects/affine.js
+++ src/mg-space2/models/parabola/projects/affine.js
@@ -8,12 +8,12 @@ define(['../../../utils/utils', 'mg-space2/utils/vec2'], function (utils, vec2) 
             getP: function () {
                 var scaleX = vec2.length(affine.basis[0][0], affine.basis[0][1]),
                     scaleY = vec2.length(affine.basis[1][0], affine.basis[1][1]);
-                return self.p * scaleX / scaleY / scaleY;
+                return self.p * scaleY * scaleY / scaleX;
             },
             setP: function (p) {
                 var scaleX = vec2.length(affine.basis[0][0], affine.basis[0][1]),
                     scaleY = vec2.length(affine.basis[1][0], affine.basis[1][1]);
-                self.p = p * scaleX / scaleY / scaleY;
+                self.p = p * scaleY / scaleX / scaleX;
             },
             get p() {
                 return this.getP();
