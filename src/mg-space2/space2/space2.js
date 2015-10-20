4a517a9bd0ed171d0aca9b0b9461d1fc1dd5a14d
sivsais <sivsais@gmail.com>
2015-10-21 02:06:42 +0300

    fix(paths):

--- src/mg-space2/space2/space2.js
+++ src/mg-space2/space2/space2.js
@@ -4,7 +4,8 @@
 
 define(['mg-space2/models/axes/axes', 'mg-space2/models/point/point','mg-space2/models/line/line',
     'mg-space2/models/segment/segment', 'mg-space2/models/curve2/curve2',
-    'mg-space2/models/parabola/parabola'], function () {
+    'mg-space2/models/parabola/parabola', 'mg-space2/models/hyperbolic/hyperbolic',
+    'mg-space2/models/ellipse/ellipse'], function () {
     var space2 = {};
     Array.prototype.forEach.call(arguments, function (item) {
         space2[item.name] = item;
